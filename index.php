6159bb32b6ba9d4e0130bff3791fbd60f4014009
Dustin <dustynewman@gmail.com>
2020-03-27 20:48:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,388 @@
+
+
+<!-- saved from url=(0024)https://dgnew.github.io/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<!-- meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>dgnew</title>
+	<!-- meta -->
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>dgnew</title>
+
+	<!-- tab img -->
+	<link rel="icon" href="./dgnew/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./dgnew/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./dgnew/favicon.ico">
+
+	<!-- css plugin -->
+	<link rel="stylesheet" href="./dgnew/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./dgnew/assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./dgnew/assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="./dgnew/assets/css/flexslider.css">
+	<link rel="stylesheet" href="./dgnew/assets/css/aos.css">
+
+	<!-- main css -->
+	<link rel="stylesheet" href="./dgnew/assets/css/style.css">
+	<link rel="stylesheet" href="./dgnew/assets/css/style-responsives.css">
+
+	<!-- google fonts -->
+	<link href="./dgnew/assets/css/style.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style>@media print {#ghostery-purple-box {display:none !important}}</style>
+</head>
+
+<body data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="0"><div style="position:fixed;top:0px;left:0px;width:0;height:0;" id="scrollzipPoint"></div>
+
+	<!-- // PRELOADER BEGIN -->
+	<div id="preloader" style="display: none;">
+		<div id="status" style="display: none;">
+			<div class="loader-revolve center">
+				<span></span>
+			</div>
+		</div>
+	</div>
+	<!-- // PRELOADER END -->
+
+	<!-- // NAVIGATION BEGIN -->
+	<div id="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="logo">
+						<a class="smoth-scroll" href="https://dgnew.herokuapp.com/#home">Rebecca.</a>
+					</div>
+				</div>
+
+				<div class="col-sm-9">
+					<div class="navigation-menu">
+						<div class="navbar">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="navbar-collapse collapse">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a class="smoth-scroll" href="https://dgnew.herokuapp.com/#about">About</a></li>
+									<li><a class="smoth-scroll" href="https://dgnew.herokuapp.com/#services">Skills</a></li>
+									<li><a class="smoth-scroll" href="https://dgnew.herokuapp.com/#portfolio">Certifications</a></li>
+									<li><a class="smoth-scroll" href="https://dgnew.herokuapp.com/#contact">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // NAVIGATION END -->
+
+	<!-- // HOME BEGIN -->
+	<div id="home" class="home-bg" data-stellar-background-ratio="0.6" style="background-position: 0px 0px;">
+		<div class="overlay">
+			<div class="container">
+				<div class="row">
+					<div class="home-text">
+						<div class="profile-pic aos-init aos-animate" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0">
+							<img src="./dgnew/avatar.png">
+						</div>
+
+						<div data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0" class="aos-init aos-animate">
+							<h1>Dustin Newman</h1>
+							<h3 id="text-rotator">
+								<div style="display: none;">Security Governance</div>
+								<div style="display: none;">Data Loss Prevention</div>
+								<div style="display: block; opacity: 0.684062;">Intelligence Analysis</div>
+							</h3>
+						</div>
+					</div>
+					<a class="smoth-scroll" href="https://dgnew.herokuapp.com/#about"><i class="fa fa-angle-down arrow lift"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // HOME END -->
+
+	<!-- // ABOUT BEGIN -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="description col-md-6 col-sm-4 col-xs-12">
+					<h2>Bio</h2>
+					<p>My background is in security governance, data loss prevention,
+					<br>intelligence analysis, application development, and IT operations.
+					<br>Formal education includes system & network administration 
+					<br>as well as information science.
+					</p>
+
+					<ul class="social-icons">
+						<li><a href="https://github.com/dgnew" target="_blank"><i class="fa fa-github"></i></a></li>
+						<li><a href="https://twitter.com/dgnew" target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/dustynewman/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+
+					<p>
+						<a href="dgnew.github.io/DustinNewman_Resume2020.pdf" target="_blank" class="btn btn-dark with-arrow smoth-scroll">My Resume
+							<i class="fa fa-download"></i>
+						</a>
+					</p>
+				</div>
+
+				<div class="facts col-md-5 col-sm-5 col-xs-12">
+					<div class="row">
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller scrollzip roller-title-number-1" data-slno="1" data-min="0" data-max="5" data-delay="2" data-increment="1">5</div>
+								<div class="count-name-intro">Years as a Security Analyst / Engineer</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-2 scrollzip" data-slno="2" data-min="0" data-max="5" data-delay="2" data-increment="1">5</div>
+								<div class="count-name-intro">Years of Intel Experience</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-3 scrollzip" data-slno="3" data-min="0" data-max="12" data-delay="2" data-increment="1">12</div>
+								<div class="count-name-intro">Years Combined Active / Reserve Military Service</div>
+							</div>
+						</div>						
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-4 scrollzip" data-slno="4" data-min="0" data-max="12" data-delay="2" data-increment="1">12</div>
+								<div class="count-name-intro">Years of App Dev / IT Ops Experience</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // ABOUT END -->
+
+	<!-- // SERVICES BEGIN -->
+	<section id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Skills</h2>
+				</div>
+
+				<div class="col-md-12">
+					<div class="row">
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-lock fa-2x"></i>
+								<h4>Security Background</h4>
+								<p>I've been working within the security field and in sensitive environments for most of my career.  I've maintained a security clearance and have been trusted to work with and protect sensitive government, financial, and personal information. I've stood up and tuned security tools, created and tested policies, and evaluated and reported enterprise security maturity.</p>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-commenting-o fa-2x"></i>
+								<h4>Strong Communicator</h4>
+								<p>I pride myself on being a clear and effective communicator. I have experience working directly with and developing reporting for senior management/leadership. I have received training in researching within short time frames and pulling out actionable information and presenting that information to senior decision makers.</p>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-book fa-2x"></i>
+								<h4>Love of Learning</h4>
+								<p>I've worked on everything from enterprise-wide governance and risk to COBOL development. Part of what drives my varied background is my desire to keep learning and growing my toolbox. I'm currently pursuing my Master's in Information Science and it's sparked more recent interests in data visualization and machine learning. I'm also currently growing my knowledge in the web app / cloud security space.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // SERVICES END -->
+
+	<!-- // PORTFOLIO BEGIN -->
+	<section id="portfolio">
+		<div class="container">
+			<div class="row">
+				<div data-aos="fade-up" class="aos-init">
+					<div class="col-md-12 text-center">
+						<h2>Education & Certifications</h2>
+					</div>
+
+					<div class="col-md-12 col-sm-12">
+						<div class="filter-btn col-md-12 text-center">
+							<div class="filter active" data-filter="all">All</div>
+							<div class="filter" data-filter=".completed">Completed</div>
+							<div class="filter" data-filter=".ongoing">Ongoing</div>
+							<div class="filter lst-cld" data-filter=".upcoming">Upcoming</div>
+						</div>
+
+						<div id="port-image" class="container">
+							<div class="row">
+
+								<div class="completed grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://www.youracclaim.com/badges/aff07aa2-bbe0-47a8-bfd1-78b93238c3aa" target="_blank">
+										<figure class="port-desc">
+											<img src="./dgnew/certificate-1.png" class="img-responsive" alt="Work 1">
+											<figcaption>
+												<h4>Certified Information Systems Security Professional</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="completed grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://www.bellevue.edu/degrees/bachelor/system-network-admin-bs/" target="_blank">
+										<figure class="port-desc">
+											<img src="./dgnew/certificate-2.png" class="img-responsive" alt="Work 2">
+											<figcaption>
+												<h4>Bachelor's in System and Network Administration</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="ongoing grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://ualr.edu/informationscience/ms-in-information-science/" target="_blank">
+										<figure class="port-desc">
+											<img src="./dgnew/certificate-3.png" class="img-responsive" alt="Work 3">
+											<figcaption>
+												<h4>Master's in Information Science</h4>
+												<h5>Ongoing</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // PORTFOLIO END -->
+
+
+	<!-- // CONTACT BEGIN -->
+	<section id="contact">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h2>Contact Me</h2>
+				</div>
+
+				<div class="address-icons col-md-8 col-md-offset-2">
+					<div class="col-md-4 col-sm-4">
+						<i class="fa fa-envelope-o fa-2x"></i>
+						<p>dustynewman@gmail.com</p>
+					</div>
+
+					<div class="col-md-4 col-sm-4">
+						<!-- <i class="fa fa-map-marker fa-2x"></i>
+						<p>Banguntapan,
+							<br> Yogyakarta, ID 55198
+						</p> -->
+					</div>
+
+					<div class="col-md-4 col-sm-4">
+						<i class="fa fa-whatsapp fa-2x"></i>
+						<p>4029906781</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+
+				<div class="col-md-8 col-md-offset-2">
+					<!-- changes your email domain in mailer.php (assets/php/) -->
+					<form id="ajax-contact" name="contact" method="post" action="https://dgnew.herokuapp.com/mailer.php">  
+						
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<div class="form-group">
+									<input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
+									<p class="help-block text-danger"></p>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6">
+								<div class="form-group">
+									<input type="email" name="email" id="email" class="form-control" placeholder="Email" required="">
+									<p class="help-block text-danger"></p>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<textarea name="message" id="message" class="form-control" rows="4" placeholder="Message..." required=""></textarea>
+							<p class="help-block text-danger"></p>
+						</div>
+						<div class="col-md-12 col-xs-12">
+							<button id="submit" type="submit" name="submit" class="btn btn-dark">Send Message</button>
+						</div>
+						<div id="form-messages" class="col-md-12 col-xs-12"></div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // CONTACT END-->
+
+	<!-- // FOOTER BEGIN -->
+	<footer id="footer">
+		<div class="container">
+			<div class="col-md-12 text-center">
+				<ul class="social-icons-footer">
+					<li><a href="https://github.com/dgnew/" target="_blank"><i class="fa fa-github"></i></a></li>
+					<li><a href="https://twitter.com/dustynewman" target="_blank"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/dustynewman/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+			</ul>
+
+				<p>© 2020</p>
+			</div>
+		</div>
+	</footer>
+	<!-- // FOOTER END -->
+
+
+
+
+	<!-- js plugin -->
+	<script src="./dgnew/jquery.js"></script>
+	<script src="./dgnew/bootstrap.min.js"></script>
+	<script src="./dgnew/jquery.stellar.min.js"></script>
+	<script src="./dgnew/text-rotate.js"></script>
+	<script src="./dgnew/numscroller.js"></script>
+	<script src="./dgnew/jquery.appear.js"></script>
+	<script src="./dgnew/progress-bar.min.js"></script>
+	<script src="./dgnew/jquery.mixitup.js"></script>
+	<script src="./dgnew/jquery.magnific-popup.min.js"></script>
+	<script src="./dgnew/magnific-popup-options.js"></script>
+	<script src="./dgnew/jquery.flexslider-min.js"></script>
+	<script src="./dgnew/aos.js"></script>
+	<script src="./dgnew/app.js"></script>
+
+	<!-- main js -->
+	<script src="./dgnew/main.js"></script>
+
+
+</body></html>
